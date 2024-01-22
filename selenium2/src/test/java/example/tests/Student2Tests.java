940769e7f59d0ebd1b80600ab278cb305d96d2e6
ZivanicBojan <zivanic.sv61.2021@uns.ac.rs>
2024-01-22 18:38:06 +0100

    Finished testing cancel reservations

--- selenium2/src/test/java/example/tests/Student2Tests.java
+++ selenium2/src/test/java/example/tests/Student2Tests.java
@@ -3,12 +3,14 @@ package example.tests;
 import example.pages.AvailabilityTestPages.OwnerAccommodationsPage;
 import example.pages.HomePage;
 import example.pages.LogInPage;
+import example.pages.ReservationTestPages.GuestReservationsPage;
 import example.pages.ReservationTestPages.OwnerReservationsPage;
 import org.testng.annotations.Test;
 
 public class Student2Tests extends TestBase {
 
-    static final String EMAIL = "owner@gmail.com";
+    static final String OWNER_EMAIL = "owner@gmail.com";
+    static final String GUEST_EMAIL="guest@gmail.com";
     static final String PASSWORD = "123";
 
     private static String PAGE_URL="http://localhost:4200";
@@ -21,7 +23,7 @@ public class Student2Tests extends TestBase {
         HomePage home = new HomePage(driver);
         home.clickOnLogIn();
 
-        LogInPage logInPage = new LogInPage(driver,EMAIL,PASSWORD);
+        LogInPage logInPage = new LogInPage(driver,OWNER_EMAIL,PASSWORD);
         logInPage.LogIn();
 
         HomePage homePage = new HomePage(driver);
@@ -38,6 +40,23 @@ public class Student2Tests extends TestBase {
 
     }
 
+    @Test
+    public void testGuestDenyReservation() throws InterruptedException {
+        driver.get(PAGE_URL);
+        //Create object of HomePage Class
+        HomePage home = new HomePage(driver);
+        home.clickOnLogIn();
+
+        LogInPage logInPage = new LogInPage(driver, GUEST_EMAIL, PASSWORD);
+        logInPage.LogIn();
+
+        HomePage homePage = new HomePage(driver);
+
+        homePage.clickOnReservations();
+        
+        GuestReservationsPage guestReservationsPage = new GuestReservationsPage(driver);
+        guestReservationsPage.clickCancelButtonForSecondRow();
+    }
 
 
 }
