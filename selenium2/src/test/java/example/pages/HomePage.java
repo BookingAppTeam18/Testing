7a13feb02ac8f17948f774b040cb3349206987fd
Katarina Krstin <92788177+kkatarinna@users.noreply.github.com>
2024-01-22 21:43:17 +0100

    added test for selenium

--- selenium2/src/test/java/example/pages/HomePage.java
+++ selenium2/src/test/java/example/pages/HomePage.java
@@ -1,12 +1,18 @@
 package example.pages;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomePage {
     private WebDriver driver;
@@ -19,8 +25,31 @@ public class HomePage {
     @FindBy(css = "button.location-icon")
     private WebElement ownerAccommodations;
 
-    @FindBy(css = "mat-form-field.search-input input")
+    @FindBy(css = "mat-form-field input")
     private WebElement searchInput;
+
+    @FindBy(css = "button.filter-icon")
+    private WebElement filterButton;
+
+    @FindBy(css = "button.filter-confirm")
+    private WebElement confirmFilters;
+
+    @FindBy(css = "input.slide-price-left")
+    private WebElement priceSlider;
+
+    @FindBy(css = ".number-of-guests mat-form-field input")
+    private WebElement numberOfGuestsFilter;
+
+    @FindBy(css = "input.date-of-start")
+    private WebElement startDate;
+
+    @FindBy(css = "input.date-of-end")
+    private WebElement endDate;
+
+    @FindBy(id = "cards")
+    private WebElement cardsContainer;
+
+
 
     public HomePage(WebDriver driver){
         this.driver=driver;
@@ -39,8 +68,144 @@ public class HomePage {
 
     }
 
-    public void performSearch() {
-        searchInput.click();
-        searchInput.sendKeys("Da");
+    public void performSearchGood() {
+//        searchInput.click();
+        searchInput.clear();
+        searchInput.sendKeys("kuce");
+        searchInput.sendKeys(Keys.ENTER);
     }
+
+    public void peroformSearchBad(){
+        searchInput.clear();
+        searchInput.sendKeys("qwe");
+        searchInput.sendKeys(Keys.ENTER);
+    }
+
+    public void clickOnFilterButton(){
+        (new WebDriverWait(driver, 10))
+                .until(ExpectedConditions.elementToBeClickable(filterButton)).click();
+
+    }
+
+    public void editNumberOfGuests(String number) {
+        numberOfGuestsFilter.clear();
+        numberOfGuestsFilter.sendKeys(number);
+        numberOfGuestsFilter.sendKeys(Keys.ENTER);
+    }
+
+    public void confirmFilters(){
+        (new WebDriverWait(driver, 10))
+                .until(ExpectedConditions.elementToBeClickable(confirmFilters)).click();
+
+    }
+
+    public void performSlidingPrice(){
+        Actions actions = new Actions(driver);
+
+        // Kalkulacija vrednosti koju želite postaviti na slajderu (u ovom slučaju 500)
+        int desiredValue = 150;
+
+        // Dobavljanje širine slajdera
+        int sliderWidth = priceSlider.getSize().getWidth();
+
+        // Izračunavanje x-offseta za postavljanje vrednosti
+        double xOffset = sliderWidth * ((double) desiredValue / 1000);
+
+        // Pomeranje do određenog x-offseta koristeći Actions
+        actions.clickAndHold(priceSlider).moveByOffset((int) xOffset, 0).release().perform();
+
+    }
+
+    public void enterStartAndEndDates() throws InterruptedException {
+        String start = "01/15/2024";
+        String end = "01/20/2024";
+
+        Actions actions = new Actions(driver);
+
+        actions.moveToElement(startDate).click().perform();
+        // Simulirajte kombinaciju tastera za brisanje teksta
+        actions.keyDown(Keys.COMMAND).sendKeys("a").keyUp(Keys.COMMAND).sendKeys(Keys.BACK_SPACE).perform();
+
+        // Selektujte end datum
+        actions.moveToElement(endDate).click().perform();
+        actions.keyDown(Keys.COMMAND).sendKeys("a").keyUp(Keys.COMMAND).sendKeys(Keys.BACK_SPACE).perform();
+
+
+        startDate.sendKeys(start);
+        endDate.sendKeys(end);
+    }
+
+    public void performSelectingType(){
+        WebElement matSelectElement = driver.findElement(By.className("types"));
+
+        // Otvorite mat-select
+        matSelectElement.click();
+
+        // Sačekajte da se pojavi opcija "Apartment"
+        By optionLocator = By.xpath("//mat-option/span[contains(text(), 'Apartment')]");
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        WebElement optionApartment = wait.until(ExpectedConditions.visibilityOfElementLocated(optionLocator));
+
+        // Kliknite na željenu opciju
+        optionApartment.click();
+    }
+
+    public void performSelectingLocation(){
+        WebElement matSelectElement = driver.findElement(By.className("lokacijice"));
+
+        // Otvorite mat-select
+        matSelectElement.click();
+
+        // Sačekajte da se pojavi opcija "Apartment"
+        By optionLocator = By.xpath("//mat-option/span[contains(text(), 'Novi Sad')]");
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        WebElement optionApartment = wait.until(ExpectedConditions.visibilityOfElementLocated(optionLocator));
+
+        // Kliknite na željenu opciju
+        optionApartment.click();
+    }
+
+    public void performSelectingBenefit(){
+        WebElement benefiteDiv = driver.findElement(By.id("iconTest"));
+
+        WebElement innerDiv = benefiteDiv.findElement(By.tagName("div"));
+
+
+        List<WebElement> childrenElements = innerDiv.findElements(By.xpath("./*"));
+
+        childrenElements.get(0).click();
+
+    }
+
+    public List<String> getCards() {
+        // Pronađi roditeljski element "cards"
+//        WebElement cardsContainer = driver.findElement(By.id("cards"));
+
+        // Pronađi sve elemente s klasom "card" unutar "cards" kontejnera
+        List<WebElement> cardElements = cardsContainer.findElements(By.className("card"));
+
+        // Sada možete izvesti radnje s pronađenim elementima
+        List<String> names = new ArrayList<String>();
+        for (WebElement cardElement : cardElements) {
+            // Radnje s pojedinim cardElement
+            WebElement h4Element = cardElement.findElement(By.tagName("h4"));
+            String accommodationName = h4Element.getText();
+            System.out.println("Ime smještaja: " + accommodationName);
+            names.add(accommodationName);
+        }
+        return names;
+    }
+
+    public WebElement getSearchInput() {
+        return searchInput;
+    }
+
+    public WebElement getFilterButton(){
+        return filterButton;
+    }
+
+    public WebElement cardsVisible(){
+        return cardsContainer;
+    }
+
 }
--- selenium2/src/test/java/example/tests/Student3Tests.java
+++ selenium2/src/test/java/example/tests/Student3Tests.java
@@ -3,19 +3,27 @@ package example.tests;
 import example.pages.HomePage;
 import example.pages.LogInPage;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 public class Student3Tests extends TestBase{
 
     static final String EMAIL = "guest@gmail.com";
     static final String PASSWORD = "123";
-    private static String PAGE_URL = "http://localhost:4200";
+    private static final String PAGE_URL = "http://localhost:4200";
 
     @Test
-    public void testSearchAndFilter() {
+    public void testSearchAndFilter() throws InterruptedException {
         driver.get(PAGE_URL);
+
+        WebDriverWait wait = new WebDriverWait(driver, 10);
 
         HomePage home = new HomePage(driver);
         home.clickOnLogIn();
@@ -23,14 +31,61 @@ public class Student3Tests extends TestBase{
         LogInPage logInPage = new LogInPage(driver, EMAIL, PASSWORD);
         logInPage.LogIn();
 
-        WebDriverWait wait = new WebDriverWait(driver, 10);
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.name("email")));
 
 
         HomePage homePage = new HomePage(driver);
+//        Thread.sleep(3000);
+        wait.until(ExpectedConditions.visibilityOf(homePage.cardsVisible()));
+        wait.until(ExpectedConditions.visibilityOf(homePage.getSearchInput()));
 
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.name("searchInput")));
 
-        homePage.performSearch();
+        //Provera za search
+        homePage.performSearchGood();
+        wait.until(ExpectedConditions.visibilityOf(homePage.cardsVisible()));
+        checkPassesGoodSearch(homePage);
+//        Thread.sleep(3000);
+        wait.until(ExpectedConditions.visibilityOf(homePage.getSearchInput()));
+
+        //Provera za search kad treba da ne vrati nista
+        homePage.peroformSearchBad();
+        checkPassesBadSearch(homePage);
+
+        //refres da bi vratili sve accommodatione
+        driver.navigate().refresh();
+//        Thread.sleep(3000);
+        wait.until(ExpectedConditions.visibilityOf(homePage.cardsVisible()));
+        wait.until(ExpectedConditions.visibilityOf(homePage.getFilterButton()));
+
+        //Provera za filtere
+        homePage.clickOnFilterButton();
+        homePage.editNumberOfGuests("6");
+        homePage.performSlidingPrice();
+        homePage.enterStartAndEndDates();
+        homePage.performSelectingType();
+        homePage.performSelectingLocation();
+        homePage.performSelectingBenefit();
+        homePage.confirmFilters();
+        wait.until(ExpectedConditions.visibilityOf(homePage.cardsVisible()));
+        checForFilters(homePage);
+    }
+
+    public void checkPassesGoodSearch(HomePage homePage){
+        List<String> expected = new ArrayList<>();
+        expected.add("Apartmani za kuce");
+        List<String> actual = homePage.getCards();
+        assertEquals(actual, expected);
+    }
+
+    public void checkPassesBadSearch(HomePage homePage){
+        List<String> expected = new ArrayList<>();
+        List<String> actual = homePage.getCards();
+        assertEquals(actual, expected);
+    }
+
+    public void checForFilters(HomePage homePage){
+        List<String> expected = new ArrayList<>();
+        expected.add("Apartman 3");
+        List<String> actual = homePage.getCards();
+        assertEquals(actual, expected);
     }
 }
